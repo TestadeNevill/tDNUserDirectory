3aee4c87f8bb87516c8d25eb4458ac6d43b0276f
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-10-12 17:16:49 -0400

    Test commit

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -7,7 +7,8 @@ import API from "../utils/API";
 export default class DataArea extends Component {
   state = {
     users: [],
-    filteredUsers: []
+    filteredUsers: [],
+    sortUsers: "ascend"
   };
 
   headings = [
@@ -29,11 +30,42 @@ export default class DataArea extends Component {
     })
     this.setState({
       filteredUsers: filtered,
-
-
     })
   };
 
+  handleSortChange = heading => {
+    if (this.state.sortUsers === "ascend") {
+      this.setState({
+        sortUsers: "descend",
+      })
+
+    }
+    else {
+      this.setState({
+        sortUsers: "acsend",
+      })
+    }
+    const CompareSort = (value1, value2) => {
+      if (this.state.sortUsers === "ascend") {
+        if (heading === "name") {
+          return value1[heading].first.localCompare(value2[heading].first);
+        } else {
+          return value1[heading] - value2[heading]
+        }
+      }
+      else {
+        if (heading === "name") {
+          return value2[heading].first.localCompare(value1[heading].first);
+        } else {
+          return value2[heading] - value1[heading]
+        }
+      }
+    }
+    const sortedValue = this.state.filteredUsers.sort(CompareSort);
+    this.setState({
+      filteredUsers: sortedValue
+    })
+  }
 
   componentDidMount() {
     API.allUsers().then((value) => {
